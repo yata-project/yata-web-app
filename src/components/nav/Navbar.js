bb82fa19d7f5b5bdd4ce15d65097e5be397659b4
Winston Yeung <yeung.winston@gmail.com>
2021-01-04 19:33:27 -0500

    Implement Authentication Hook
    This change implements a hook that can be used to check whether
    the current user is authenticated or not. The approach is based off of
    the following implementations:
    
    https://usehooks.com/useAuth/
    https://reactrouter.com/web/example/auth-workflow

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,20 +1,39 @@
 import {
+  useAuth
+} from "../../hooks/UseAuth"
+
+import {
     Link
 } from 'react-router-dom'
 
 function NavBar() {
+    const auth = useAuth();
+
     return (
-        <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+        <nav class="navbar is-primary" role="navigation" aria-label="main navigation">          
           <div class="navbar-brand">
             <Link class="navbar-item has-text-weight-bold" to="/">YATA</Link>
           </div>
 
-          <div class="navbar-start">
-            <Link to="/lists" class="navbar-item">My Lists</Link>
-          </div>
+          { auth.user &&
+            <div class="navbar-start">
+              <Link to="/lists" class="navbar-item">My Lists</Link>
+            </div>
+          }
 
           <div class="navbar-end">
-            <Link to="/login" class="navbar-item">Log In</Link>
+            <div class="navbar-item">
+              {
+                auth.user && <div class="navbar-item">Welcome, {auth.user.name}</div>
+              }
+              {
+                auth.user ? (
+                  <button onClick={() => auth.signout()} class="navbar-item">Log Out</button>
+                ) : (
+                  <button onClick={() => auth.signin()} class="navbar-item">Log In</button>
+                )
+              }
+            </div>
           </div>
         </nav>
         );
