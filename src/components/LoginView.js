cfca35d0ed648f672f23841e8cd2435640ca7b4b
Winston Yeung <yeung.winston@gmail.com>
2021-01-04 21:32:04 -0500

    Integrate authentication with AWS Cognito using AWS Amplify
    This commit integrates the authentication piece with Cognito via AWS Amplify.
    
    It attempts to use duck typing for the Amplify Authentication module in order to
    make it easy to change the backend for the authentication piece.

---
+++ src/components/LoginView.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useAuth } from "../hooks/UseAuth";
+import * as AuthProvider from '../AmplifyAuthProvider';
+
+function LoginView() {
+    const auth = useAuth();
+
+    React.useEffect(() => {
+        AuthProvider.listenForAuthStateChange(auth.signin, auth.signout)
+    }, [auth]);
+
+    return (
+        <div class="container">{AuthProvider.getSigninComponent()}</div>
+    )
+}
+
+export default LoginView;
