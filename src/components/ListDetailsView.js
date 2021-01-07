b57f0fce3ed9de0a3d847c23530e6d6edd28f8c9
Harrison H. Jones <harrison@hhj.me>
2021-01-06 20:55:04 -0500

    Format project with prettier

--- src/components/ListDetailsView.js
+++ src/components/ListDetailsView.js
@@ -1,20 +1,18 @@
 import React from "react";
-import {
-    useParams
-} from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 /**
  * ListDetailsView is the view which shows the details for a list and all its items
  */
 function ListDetailsView() {
-    let { id } = useParams();
+  let { id } = useParams();
 
-    return (
-        <div>
-            <h3>ID: {id}</h3>
-            This will be the page that displays all the items and details for a list
-        </div>
-    )
+  return (
+    <div>
+      <h3>ID: {id}</h3>
+      This will be the page that displays all the items and details for a list
+    </div>
+  );
 }
 
 export default ListDetailsView;
