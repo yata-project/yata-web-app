e80cd43cc54c58111e743650f7ae826a7918440b
Harrison H. Jones <harrison@hhj.me>
2021-01-30 14:12:02 -0500

    Add CDK for deployment

---
+++ cdk/bin/cdk.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import "source-map-support/register";
+
+import * as cdk from "@aws-cdk/core";
+
+import { CdkStack } from "../lib/cdk-stack";
+
+const app = new cdk.App();
+new CdkStack(app, "YataWebApp", {
+  env: {
+    region: "us-east-1",
+  },
+  terminationProtection: true,
+});
---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,42 @@
+import * as cdk from "@aws-cdk/core";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as s3Deploy from "@aws-cdk/aws-s3-deployment";
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // S3:
+    const bucket = new s3.Bucket(this, "Bucket", {
+      // blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // Deployment:
+    const src = new s3Deploy.BucketDeployment(this, "Deployment", {
+      sources: [s3Deploy.Source.asset("../build")],
+      destinationBucket: bucket,
+    });
+
+    // Allow CF to access the S3 bucket:
+    const oai = new cloudfront.OriginAccessIdentity(
+      this,
+      "OriginAccessIdentity",
+      {}
+    );
+
+    // Cloudfront:
+    const cf = new cloudfront.CloudFrontWebDistribution(this, "Distribution", {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: bucket,
+            originAccessIdentity: oai,
+          },
+          behaviors: [{ isDefaultBehavior: true }],
+        },
+      ],
+    });
+  }
+}
